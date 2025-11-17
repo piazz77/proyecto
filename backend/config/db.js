751418159877b79bd100b81708c78fed3a83507b
Guadito28 <guadalupe.santillan.2804gmail.com>
2025-11-17 10:21:12 -0300

    tus cambios

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,12 +4,15 @@ dotenv.config();
 
 const connection = mysql.createConnection({
   
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
 
-
+ host:"LocalHost",
+ user:"root",
+ password:"guada2804.",
+ database:"simulador_lamparas"
   
   
 });
